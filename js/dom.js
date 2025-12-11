6fa526e4630948e81615b5c3e195eaeeadcac256
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-11 15:21:43 +0600

    addad for off in js file

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,10 @@
 console.log(document);
 console.log("hello from js");
+const liCollection = document.getElementsByTagName("li");
+for (const li of liCollection) {
+    console.log(li.innerText);
+}
+const allHeading = document.getElementsByTagName("h1");
+for (const h1 of allHeading) {
+    console.log(h1);
+}
