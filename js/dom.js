66d78c75948a6b65f250cb3ebee34d5e5e46a966
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-13 00:19:17 +0600

    addad style in js file

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,8 @@
-
+const sections =document.querySelectorAll("section")
+for(const section of sections){
+    section.style.border ="2px solid blue"
+    section.style.paddingLeft = "10px   "
+    section.style.marginBottom = "5px"
+    section.style.borderRadius= "20px"
+    section.style.background = "lightgray"
+}
