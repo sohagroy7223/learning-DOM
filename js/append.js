b8a48fd9043c66a85c707cb44084369d23c154ee
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-14 13:18:08 +0600

    addad a full section in page on append proprty

--- js/append.js
+++ js/append.js
@@ -1,4 +1,41 @@
+//1. where to add 
 const placeList = document.getElementById("place-list")
+//2. what to be add
 const li = document.createElement("li")
 li.innerText="salbon"
+
+//3. add the child
 placeList.appendChild(li)
+
+// new section add by js**********
+
+//1. where to add
+const mainContainer = document.getElementById("main-container")
+console.log(mainContainer)
+
+//2.what to be add
+const section = document.createElement("section");
+const h1 = document.createElement("h1");
+h1.innerText="My food item";
+section.appendChild(h1);
+
+
+const ul = document.createElement("ul")
+section.appendChild(ul)
+const li1 = document.createElement("li")
+li1.innerText = "polaow"
+ul.appendChild(li1)
+
+const li2 = document.createElement("li")
+li2.innerText = "biryani"
+ul.appendChild(li2)
+
+const li3 = document.createElement("li")
+li3.innerText = "borhanay"
+ul.appendChild(li3)
+
+const li4 = document.createElement("li")
+li4.innerText = "doi"
+ul.appendChild(li4)
+
+mainContainer.appendChild(section);
---
+++ js/styleInJs.js
@@ -0,0 +1,19 @@
+const sections =document.querySelectorAll("section")
+for(const section of sections){
+    section.style.border ="2px solid blue"
+    section.style.paddingLeft = "10px   "
+    section.style.marginBottom = "5px"
+    section.style.borderRadius= "20px"
+    section.style.background = "lightgray"
+}
+const places = document.getElementsByClassName("place-container")
+for(const place of places){
+    place.style.textAlign = "center"
+    place.style.fontSize = "1.5rem"
+}
+
+
+const nav = document.getElementById("nav-container");
+nav.classList.add("home-page")
+nav.classList.remove("home-page")
+nav.lastChild
