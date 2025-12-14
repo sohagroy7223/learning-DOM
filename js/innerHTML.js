d52846835cebaa295ce39c7f4a1bbbf2582811f4
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-14 21:20:18 +0600

    addad a new section in this page by the innerHTML on inner html

---
+++ js/innerHTML.js
@@ -0,0 +1,19 @@
+const maine= document.getElementById("main-container") 
+
+const section1 = document.createElement("section")
+section1.innerHTML = `
+ 
+<h1>my dynamic day 3</h1>
+<p>this is create by js and now it's work</p>
+<ul>
+    <li>sunday</li>
+    <li>monday</li>
+    <li>tuesday</li>
+    <li>wednesday</li>
+    <li>thursday</li>
+</ul>
+`
+maine.appendChild(section1)
+
+
+
